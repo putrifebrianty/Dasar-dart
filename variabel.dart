31c59698bf5c5fdc5f32865417d9e0c08fc67207
putrifebrianty <78517974+putrifebrianty@users.noreply.github.com>
2021-02-18 16:09:06 +0700

    Add files via upload

---
+++ variabel.dart
@@ -0,0 +1,12 @@
+void main() {
+  String nama = "Putri Febrianty";
+  int umur = 21;
+  double tinggi = 155.0;
+  bool isMenikah = false;
+
+  var alamat = "Medan, Indonesia";
+
+  print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+  print("Menikah: $isMenikah");
+  print("Alamat : $alamat");
+}
